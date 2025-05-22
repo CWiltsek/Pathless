820390c641e66acb02c72ef6cd4bdb0dc045554a
Casey Wiltsek <casey.wiltsek@gmail.com>
2025-05-22 10:19:34 -0400

    Added a basic map to Map class and gave the Player an equipped_item variable to show the item the are actively using

--- Player.py
+++ Player.py
@@ -1,11 +1,16 @@
-# Creating the Player class  to store the players name, race, stats, and items
+# Imports everything from the Items file for the Player class to use
+from Items import *
+
+# Creating the Player class  to store the players name, race, stats, location, and items
 class Player:
-    
+
     races = ['Elf', 'Ork', 'Human', 'Wizard']
 
-    # Contructor for the Player class where the player can choose a name and rae and have their stats and items stored
+    # Contructor for the Player class where the player can choose a name and race and have their stats, location, and items stored in their bag
     def __init__(self, name, race):
         self.name = name
         self.race = race
         self.stats = {'health': 10, 'attack': 10, 'defense': 10, 'speed': 10, 'intelligence': 10}
-        self.items = []
+        self.backpack = []
+        self.equipped_item = Items()
+        self.location = [0, 0]
