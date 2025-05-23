562693d5cee56e3e94751cf4762edfbe553a6d46
Casey Wiltsek <casey.wiltsek@gmail.com>
2025-05-23 09:40:32 -0400

    Edit map layout to make it bigger, started the the function to move the player, began the start function in main in Pathless to create a player

--- Pathless.py
+++ Pathless.py
@@ -9,4 +9,15 @@ class main:
 
     # Function to initialize the game
     def start():
-        pass
+        print('Welcome to the dungeon!')
+        name = input('Please choose your character\'s name. ')
+        if not type(name) == str:
+            print('Invalid name choice. Must be a valid string. ')
+            name = input('Please choose your character\'s name. ')
+        race = input('Now choose a race between Elf, Ork, Human, or Wizard. ')
+        if not race.lower() in ['elf', 'human', 'ork', 'wizard']:
+            print('Invalid race choice. ')
+            race = input('Please choose a race between Elf, Ork, Human, or Wizard. ')
+        player = Player(name, race)
+
+    start()
--- Player.py
+++ Player.py
@@ -15,3 +15,17 @@ class Player:
         self.equipped_item = Items.no_item
         self.x = 1
         self.y = 1
+
+    def move(self, direction, distance):
+        distance_remaining = distance
+        if direction.lower() == 'north':
+            if self.y == 1 and distance_remaining > 0:
+                print('You have reached a wall. Please choose another direction and distance.')
+                new_direction = input('Please specify a direction. ')
+                new_distance = input('Now specify a distance. ')
+                if not type(new_direction) == str or not new_direction.lower() in ['north', 'east', 'south', 'west']:
+                    print('Invalid directions choice. Please choose between north, south, east, or west. ')
+                    new_direction = input('Please specify a direction. ')
+                if not type(new_distance) == int or not new_distance < 29:
+                    print('Invalid distance. Please choose a distance between 1 and 28. ')
+                    new_distance = input('Now specify a distance. ')
